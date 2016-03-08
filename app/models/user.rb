3e128c8a6bdf8350f4c44f874b19f716da4a9abb
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-08 15:44:53 +0800

    LoginLogOut First Commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,50 @@
 class User < ActiveRecord::Base
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  attr_accessor :remember_token
+
   has_many :active_relationships,  class_name: Relationship.name,
-                                   foreign_key: :follower_id,
-                                   dependent: :destroy
+    foreign_key: :follower_id,
+    dependent: :destroy
   has_many :passive_relationships, class_name: Relationship.name,
-                                   foreign_key: :followed_id,
-                                   dependent: :destroy
-  has_many :following, through: :active_relationships,  source: :followed
+    foreign_key: :followed_id,
+    dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
   has_many :activities
   has_many :lessons
+
+  validates :name,  presence: true, length: {maximum: 50}
+  validates :email, presence: true, length: {maximum: 255},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: {case_sensitive: false}
+  validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+
+  before_save {self.email = email.downcase}
+
+  has_secure_password
+
+  def digest string
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+      BCrypt::Engine.cost
+      BCrypt::Password.create string, cost: cost
+  end
+
+  def new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
 end

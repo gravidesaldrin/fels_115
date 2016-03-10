c3ec2f646855871bb432b6246ec11e3bcefe41ff
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-10 10:35:26 +0800

    Add namespace and delete function

---
+++ app/controllers/admin_controller.rb
@@ -0,0 +1,14 @@
+class AdminController < ApplicationController
+  before_action :require_admin
+
+  private
+  def require_admin
+    unless current_user.admin?
+      redirect_to root_path
+    end
+  end
+
+   def find_user
+    @user = User.find params[:id]
+  end
+end

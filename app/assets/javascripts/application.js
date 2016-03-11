bd924774fd6f1b50c933eb751bff243eb260372c
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-11 15:52:10 +0800

    Add follow-unfollow gui and left files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,6 +17,5 @@
 //= require_tree .
 
 $("[data-toggle=collapse]").click(function(){
-  $(this).find("i").toggleClass("glyphicon-chevron-right
-    glyphicon-chevron-down");
-});
+$(this).find("i").toggleClass("glyphicon-chevron-right
+  glyphicon-chevron-down");

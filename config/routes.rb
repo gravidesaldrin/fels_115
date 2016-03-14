15c569ae44c7e0a988eb89678f9c784ab58d6c80
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-15 00:16:05 +0800

    Finish Follow-Unfollow Button

--- config/routes.rb
+++ config/routes.rb
@@ -17,5 +17,5 @@ Rails.application.routes.draw do
 
   resources :account_activations, only: [:edit]
   resources :password_resets, only: [:new, :create, :edit, :update]
-  resources :relationships, only: [:create, :destroy]
+  resources :relationships, only: [:index, :create, :destroy]
 end

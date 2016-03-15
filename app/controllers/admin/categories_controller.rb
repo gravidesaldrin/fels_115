7fda02aa9296da45075b6b42e950520a161d75ae
Aldrin <aldrin.s.gravides@framgia.com>
2016-03-15 15:53:14 +0800

    Add category masterfile

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,50 @@
+class Admin::CategoriesController < AdminController
+  before_action :find_category, except: [:index, :new, :create]
+
+  def index
+    @categories = Category.order(:name).paginate page: params[:page]
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new category_params
+    if @category.save
+      flash[:success] = t ".success"
+      redirect_to admin_categories_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    if @category.update_attributes category_params
+      flash[:success] = t ".success"
+      redirect_to admin_category_path @category
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @category.destroy
+    redirect_to admin_categories_path
+  end
+
+  private
+  def find_category
+    @category = Category.find params[:id]
+  end
+
+  def category_params
+    params.require(:category).permit :name
+  end
+end
